60d4e053564a9669fe83eb72cec317c67b517821
Vinko <Vinko@mail.com>
2025-01-19 19:19:11 +0100

    Fix test errors

--- tests/internship-test-logged-in.spec.ts
+++ tests/internship-test-logged-in.spec.ts
@@ -10,21 +10,20 @@ import { expect, test } from '@playwright/test'
 
 test('Navigate to valamar.com & validate page title', async ({ page }) => {
 
-  const valamarURL = 'https//valamar.com'
+  const valamarURL = 'https://valamar.com' // Added missing : so that the URL is working 
   await page.goto(valamarURL)
 
   await expect(
     page,
     'Page does NOT have expected page title.'
-  ).toHaveTitle('Valamar Holiday Hotels & Resorts', { timeout: 60000 })
+  ).toHaveTitle('Valamar Holiday Hotels & Resorts in Croatia and Austria', { timeout: 60000 })//Changed Title with added part "in Croatia and Austria"
 })
 
 test('Navigate to valamar.com & click on Log in button', async ({ page }) => {
 
   const valamarURL = 'https://valamar.com'
   await page.goto(valamarURL)
-
-  await page.locator('div[id="azureb2c-login"]').click({ timeout: 30000 })
+  await page.locator('button[id="azureb2c-login"]').click({ timeout: 30000 }) // Changed div to button to locate the SIGN IN
 
   await page
     .locator('button[class="btn-vlm-primary w-full mt-6 app-button"]')
@@ -48,13 +47,13 @@ async function fetchData(url: string): Promise<Response> {
   }
 }
 
-test('Validate ALL page urls & Network response', async ({ }) => {
+test('Validate ALL page urls & Network response', async ({}) => {
   const urls = ['https://valamar.com', 'https://www.rtl.hr/', 'https://www.moemax.hr/', 'https://optika-anda.com/', 'https://www.suncanihvar.com/hr']
   console.log(urls.length)
 
   //NOTE: Make sure you're validating that ALL of urls have the appropriate structure
-  for (let i = 1; i < urls.length; i++) {
-    const url = urls[j]
+  for (let i = 0; i < urls.length; i++) {  //set starting index to 0 so that array can start from the first element
+    const url = urls[i] // fixed wrong letter
     console.log("Getting data for ", url)
     expect(url, 'URL does NOT have the proper structure!').toContain('https://')
   }
@@ -63,7 +62,7 @@ test('Validate ALL page urls & Network response', async ({ }) => {
   const response = await fetchData(urls[0])
   console.log("response\n", response)
 
-  const statusCode = '' //TODO: Replace this declaration with the actual status code from the response
+  const statusCode = response.status; //extracted status code with response.status
   expect(statusCode, 'Unexpected status code!').toEqual(200)
 
 })
